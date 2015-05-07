eb68912102a47571ced44447e7ad8fea48db5984
wavefunctionp <wavefunctionp@gmail.com>
2015-05-07 03:28:20 -0500

    inital commit

---
+++ SimpleScienceFix/SimpleScienceFix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SimpleScienceFix
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class SimpleScienceFix :MonoBehaviour
+    {
+        private void Update()
+        {
+            foreach (ModuleScienceExperiment exp in FlightGlobals.ActiveVessel.FindPartModulesImplementing<ModuleScienceExperiment>())
+            {
+                if (exp.experimentID == "crewReport" | exp.experimentID == "surfaceSample" | exp.experimentID == "evaReport")
+                {
+                    List<ModuleScienceExperiment> explist = new List<ModuleScienceExperiment>();
+                    explist.Add(exp);
+                    exp.part.Modules.GetModules<ModuleScienceContainer>()[0].StoreData(explist.Cast<IScienceDataContainer>().ToList(), true);
+                    exp.ResetExperiment();
+                    explist.Clear();
+                }
+                Debug.Log(exp.experimentID);
+            }
+        }
+
+    }
+}
